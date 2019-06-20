788278e13c61782679fc45baeadc0e7b19a31044
droidmonk <lekan.akinremi@yahoo.com>
2019-06-20 09:41:24 +0100

    tokad mart bags

---
+++ src/data.js
@@ -0,0 +1,116 @@
+/*
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Tote Handbag - Black",
+    img: "img/product-1.png",
+    price: 10,
+    company: "L&S",
+    info:
+      "Spacious interior: It features three lined and zipped compartments which allow you to organize your things efficiently. The middle compartment is roomy and features one zipped pocket and two slip pockets",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 2,
+    title: "Cross Body Shoulder Bag - Grey and White",
+    img: "img/product-2.png",
+    price: 16,
+    company: "L&S",
+    info:
+      "Colour block bag: this colour block handbag has three catchy and contrasting colours in play. Fine stitching and a modish design make it stand out",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 3,
+    title: "Tote Handbag - Grey and White",
+    img: "img/product-3.png",
+    price: 8,
+    company: "L&S",
+    info:
+      "Shoulder bag for women: this one features three compartments. The main middle one is spacious and lined. It has three wall pockets; two slip, one zip.",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 4,
+    title: "Cross Body Shoulder Bag - Black",
+    img: "img/product-4.png",
+    price: 18,
+    company: "L&S",
+    info:
+      "Crossbody bag with pockets: the interior of this cross body bag is lined, and spacious featuring two slip pockets and one zip pocket. Latest bag design: the steezy design of this cross body bag for girls is sure to win hearts",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 5,
+    title: "Effect Shoulder Bag - Coffe Snake",
+    img: "img/product-5.png",
+    price: 24,
+    company: "L&S",
+    info:
+      "This crocodile skin bag takes on a fresh and stunning turn with metallic accents added to the mix. With three generously accommodating compartments and wall pockets too, this one enables you to keep your stuff organized. Stylish and comfortable carrying handles and a glossy exterior, make this handbag worth attention.",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 6,
+    title: "Effect Shoulder Bag - Cream Snake",
+    img: "img/product-6.png",
+    price: 17,
+    company: "L&S",
+    info:
+      "This crocodile skin bag is lined with multi functional pockets and takes on a fresh and stunning turn with metallic accents added to the mix. With three generously accommodating compartments and wall pockets too, this one enables you to keep your stuff organized. Stylish and comfortable carrying handles and a glossy exterior, make this handbag worth attention.",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 7,
+    title: "Nude Flap Tassel Cross Body Bag - Nude",
+    img: "img/product-7.png",
+    price: 30,
+    company: "L&S",
+    info:
+      "Handbag with chain strap: this handbag for women comes with a comfortable chain strap. A detachable handbag faux leather strap also comes along. Branded handbag packaging: this Anna Grace product comes with branded packaging and comes in a dust bag for handbags. Colours available: black, blue, grey, ivory, and nude",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 8,
+    title: "Large Burgundy Hobo - Burgundy",
+    img: "img/product-8.png",
+    price: 2,
+    company: "L&S",
+    info:
+      "Hobo bag with pockets: lined and spacious interior of the handbag features two slip wall pockets and one zippered wall pouch, Hobo bag with adjustable strap: apart from the stylish handle, a detachable handbag strap accompanies as well ",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Tote Handbag - Black",
+  img: "img/product-1.png",
+  price: 10,
+  company: "L&S",
+  info:
+    "Spacious interior: It features three lined and zipped compartments which allow you to organize your things efficiently. The middle compartment is roomy and features one zipped pocket and two slip pockets",
+  inCart: false,
+  count: 0,
+  total: 0,
+};
